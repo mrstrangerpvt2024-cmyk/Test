dfdc271bcfd2afbf109968c6cc2bfeb78cd959f6
mrstrangerpvt2024-cmyk <mrstrangerpvt2024@gmail.com>
2026-09-15 14:29:13 +0530

    Update BOT_TOKEN to use environment variable name

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from telegram.ext import (
     Application, CommandHandler, MessageHandler, ContextTypes, filters
 )
 
-BOT_TOKEN = os.getenv("7366092021:AAFmA4jNXCwYS7GgqK4lsoynDn-2RZ4xx0c")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 if not BOT_TOKEN:
     raise RuntimeError("BOT_TOKEN environment variable is missing.")
